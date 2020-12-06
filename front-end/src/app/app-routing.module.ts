c991f873f4a92dbd3b1c16e843ea495e6891e941
mariannabourges <marianna.borgesdesouza@gmail.com>
2020-12-06 04:49:36 +0000

    (25/11) Início do formulário com relacionamentos

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { CursoListComponent } from './curso/curso-list/curso-list.component';
 import { CursoFormComponent } from './curso/curso-form/curso-form.component';
 
 import { TurmaListComponent } from './turma/turma-list/turma-list.component';
+import { TurmaFormComponent } from './turma/turma-form/turma-form.component';
 
 const routes: Routes = [
-    //Rotas no Angular NUNCA começam com /
-    {path: 'curso', component: CursoListComponent},
-    {path: 'curso/novo', component: CursoFormComponent},
-    {path: 'curso/:id', component: CursoFormComponent},
+    // Rotas no Angular NUNCA começam com /
+    { path: 'curso', component: CursoListComponent },
+    { path: 'curso/novo', component: CursoFormComponent},
+    { path: 'curso/:id', component: CursoFormComponent },
 
-    {path: 'turma', component: TurmaListComponent }
+    { path: 'turma', component: TurmaListComponent },
+    { path: 'turma/novo', component: TurmaFormComponent },
+    { path: 'turma/:id', component: TurmaFormComponent }
 ];
 
 @NgModule({
@@ -19,5 +22,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-
